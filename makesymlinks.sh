6e40753c05e0c74009ffab2f6594f4f2919e95d3
dmac <DaraMac@users.noreply.github.com>
2018-07-26 19:01:41 +0100

    Shell script now symlinks .config files too, fixed some spelling mistakes

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -12,6 +12,9 @@ dir=~/dotfiles		# dotfiles directory
 olddir=~/dotfiles_old	# old dotfiles backup directory
 files="vimrc"		# space seperated list of files/folders to symlink in homedir
 
+confdirs="i3 i3config" 	# directories in ~/.config
+conffiles=""
+
 ########
 
 # create dotfiles_old in homedir
@@ -24,10 +27,18 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "..done"
 
-# move any existing dorgiles in homedir to dotfiles_old directory, the create symlinks
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
 	echo "Moving any existing dotfiles from ~ to $olddir"
 	mv ~/.$file ~/dotfiles_old/
 	echo "Creating symlink to $file in home directory."
 	ln -s $dir/$file ~/.$file
+done
+
+# create directories for .config files to go and then symlink
+for dir in $confdirs; do
+	echo "Making directory $dir in ~/.config"
+	mkdir -p ~/.config/$dir
+	echo "Creating symlink in ~/.config/$dir/config"
+	ln -s ~/dotfiles/$dir/config ~/.config/$dir/config
 done
