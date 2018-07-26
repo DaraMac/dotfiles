df8b2205096a3eb04f4dc103bcfa0be2a28c6380
dmac <DaraMac@users.noreply.github.com>
2018-07-26 19:33:42 +0100

    Fixed variable name overlap which broke vimrc symlink, added full backup copying of .config files

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,23 +8,26 @@
 
 ######## Variables
 
-dir=~/dotfiles		# dotfiles directory
+dotdir=~/dotfiles	# dotfiles directory
 olddir=~/dotfiles_old	# old dotfiles backup directory
 files="vimrc"		# space seperated list of files/folders to symlink in homedir
 
-confdirs="i3 i3config" 	# directories in ~/.config
-conffiles=""
+confdirs="i3 i3status" 	# directories in ~/.config
 
 ########
 
-# create dotfiles_old in homedir
+# create dotfiles_old in homedir, along with .config directories
 echo "Creating $olddir for backup of any existing dotiles in ~"
 mkdir -p $olddir
+
+for dir in $confdirs; do
+	mkdir -p $olddir/$dir
+done
 echo "..done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
+echo "Changing to the $dotdir directory"
+cd $dotdir
 echo "..done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
@@ -32,13 +35,15 @@ for file in $files; do
 	echo "Moving any existing dotfiles from ~ to $olddir"
 	mv ~/.$file ~/dotfiles_old/
 	echo "Creating symlink to $file in home directory."
-	ln -s $dir/$file ~/.$file
+	ln -s $dotdir/$file ~/.$file
 done
 
-# create directories for .config files to go and then symlink
+# move existing files, create any new directories for .config files and symlink
 for dir in $confdirs; do
+	mv ~/.config/$dir/config $olddir/$dir/config
 	echo "Making directory $dir in ~/.config"
 	mkdir -p ~/.config/$dir
 	echo "Creating symlink in ~/.config/$dir/config"
 	ln -s ~/dotfiles/$dir/config ~/.config/$dir/config
 done
+echo "..done"
