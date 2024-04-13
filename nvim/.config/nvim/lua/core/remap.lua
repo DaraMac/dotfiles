095193164fef03800a33a2239b47ef6bf2412c4c
dmac <DaraMac@users.noreply.github.com>
2024-04-13 17:21:19 +0100

    Add most of my existing vim config into nvim, plus some other nice defaults

--- nvim/.config/nvim/lua/core/remap.lua
+++ nvim/.config/nvim/lua/core/remap.lua
@@ -1,2 +1,4 @@
-vim.g.mapleader = " "
+-- doesn't work  in the neovim version 7 that I currently have on Ubuntu
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+--
+vim.keymap.set("n", "J", "mjJ`j")
--- nvim/.config/nvim/lua/core/set.lua
+++ nvim/.config/nvim/lua/core/set.lua
@@ -1,2 +1,28 @@
-vim.wo.number = true
-vim.wo.relativenumber = true
+vim.opt.guicursor = ""
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop     = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth  = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.termguicolors = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.autowrite = true
+
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+
+vim.opt.laststatus = 0
+vim.opt.ruler = false
+
+vim.opt.tags:append('./.tags;/')
+
+vim.g.mapleader = " "
