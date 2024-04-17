bce650da8a4ba116c72413f5d18cb44005b52275
dmac <DaraMac@users.noreply.github.com>
2024-04-17 16:52:40 +0100

    nvim: Add cmd to switch to Explore without directly using the lua api as it's not in my ubuntu version yet

--- nvim/.config/nvim/lua/core/remap.lua
+++ nvim/.config/nvim/lua/core/remap.lua
@@ -1,4 +1,5 @@
--- doesn't work  in the neovim version 7 that I currently have on Ubuntu
--- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
---
-vim.keymap.set("n", "J", "mjJ`j")
+-- vim.cmd doesn't seem to work  in the neovim version 7 that I currently have on Ubuntu
+-- vim.keymap.set('n', '-', vim.cmd.Ex)
+vim.keymap.set('n', '-', '<cmd>Explore<CR>')
+
+vim.keymap.set('n', 'J', 'mjJ`j')
