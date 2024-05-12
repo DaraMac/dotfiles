a90e9b67913332fc579d2fbe6d41ce8bbbcf077f
dmac <DaraMac@users.noreply.github.com>
2024-05-12 21:59:29 +0100

    nvim: Continue adding telescope keymaps

--- nvim/.config/nvim/lua/core/lazy_init.lua
+++ nvim/.config/nvim/lua/core/lazy_init.lua
@@ -58,6 +58,13 @@ require("lazy").setup({
             vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
             vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
+            vim.keymap.set('n', '<leader>/', function()
+                -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+                builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                    winblend = 0,
+                    previewer = false,
+                })
+            end, { desc = '[/] Fuzzily search in current buffer' })
         end,
     }
 })
