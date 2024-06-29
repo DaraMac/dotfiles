76c1c7d358a35bb2e8164ea804c11523fbaee120
dmac <DaraMac@users.noreply.github.com>
2024-06-29 01:38:15 +0100

    nvim: Add todo jumps

--- nvim/.config/nvim/lua/core/lazy_init.lua
+++ nvim/.config/nvim/lua/core/lazy_init.lua
@@ -57,6 +57,7 @@ require("lazy").setup({
             vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
             vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
             vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+            vim.keymap.set("n", "<leader>st", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
             vim.keymap.set("n", "<leader>/", function()
                 -- You can pass additional configuration to Telescope to change the theme, layout, etc.
@@ -422,7 +423,22 @@ require("lazy").setup({
     -- TODO: test
     -- NOTE: here
     -- FIXME: Fix!
-    { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+    {
+        'folke/todo-comments.nvim',
+        event = 'VimEnter',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        opts = { signs = false },
+        config = function()
+            require("todo-comments").setup()
+            vim.keymap.set("n", "]t", function()
+                require("todo-comments").jump_next()
+            end, { desc = "Next todo comment" })
+
+            vim.keymap.set("n", "[t", function()
+                require("todo-comments").jump_prev()
+            end, { desc = "Previous todo comment" })
+        end,
+    },
 
     { -- Highlight, edit, and navigate code
         'nvim-treesitter/nvim-treesitter',
