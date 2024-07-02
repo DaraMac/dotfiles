6d12f7108b97706dc838c98e52f52f56358c8a9f
dmac <DaraMac@users.noreply.github.com>
2024-07-02 14:23:39 +0100

    nvim: Add Obsidian plugin

--- nvim/.config/nvim/lua/core/lazy_init.lua
+++ nvim/.config/nvim/lua/core/lazy_init.lua
@@ -473,5 +473,50 @@ require("lazy").setup({
             --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
             --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
         end,
+    },
+
+    {                  -- Obsidian
+        "epwalsh/obsidian.nvim",
+        version = "*", -- recommended, use latest release instead of latest commit
+        lazy = true,
+        ft = "markdown",
+        -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+        -- event = {
+        --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+        --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+        --   "BufReadPre path/to/my-vault/**.md",
+        --   "BufNewFile path/to/my-vault/**.md",
+        -- },
+        dependencies = {
+            -- Required.
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {
+            workspaces = {
+                {
+                    name = "notes",
+                    path = "~/College/notes",
+                },
+                {
+                    name = "wiki",
+                    path = "~/Documents/wiki",
+                },
+            },
+
+            templates = {
+                folder = "Meta/Templates"
+            },
+
+            daily_notes = {
+                folder = "Diary",
+                date_format = "%Y/Days/%Y-%m-%d-%a",
+                default_tags = {},
+                template = "Daily Note.md"
+            },
+
+            ui = {
+                enable = false,
+            }
+        },
     }
 })
