fea9983c57cee707558771ac808b9c5452b6a73a
dmac <DaraMac@users.noreply.github.com>
2024-07-30 19:30:26 +0200

    nvim: Add treesitter stuff for selection text objects

--- nvim/.config/nvim/lua/core/lazy.lua
+++ nvim/.config/nvim/lua/core/lazy.lua
@@ -493,8 +493,23 @@ require("lazy").setup({
                         keymaps = {
                             ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
                             ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-                            ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-                            ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+                            ["l="] = { query = "@assignment.lhs", desc = "Select [l]eft hand side of an assignment" },
+                            ["r="] = { query = "@assignment.rhs", desc = "Select [r]ight hand side of an assignment" },
+
+                            ["ai"] = { query = "@conditional.outer", desc = "Select outer part of an [i]f" },
+                            ["ii"] = { query = "@conditional.inner", desc = "Select inner part of an [i]f" },
+
+                            ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+                            ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+
+                            ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
+                            ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
+
+                            ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+                            ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+
+                            ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
                         }
                     },
                 },
