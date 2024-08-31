289526ac6a331afe079009ec526748b6dc9f84b6
dmac <DaraMac@users.noreply.github.com>
2024-08-31 13:41:52 +0100

    nix: Enable flakes

--- nix/configuration/configuration.nix
+++ nix/configuration/configuration.nix
@@ -7,6 +7,8 @@
             ./packages.nix
         ];
 
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
     # Bootloader.
     boot.loader.grub.enable = true;
     boot.loader.grub.device = "/dev/nvme0n1";
