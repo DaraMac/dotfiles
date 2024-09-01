c1e356ffc56d83cd29753b6e1da9ddeb86a599a4
dmac <DaraMac@users.noreply.github.com>
2024-09-01 12:16:13 +0100

    nix: Try setting up cosmic desktop

--- nix/configuration/flake.nix
+++ nix/configuration/flake.nix
@@ -1,20 +1,33 @@
 {
-  description = "System NixOS flake";
+    description = "System NixOS flake";
 
-  inputs = {
-    # NixOS official package source, using the nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+    inputs = {
+        # NixOS official package source, using the unstable branch here
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    # Please replace my-nixos with your hostname
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        # Import the previous configuration.nix we used,
-        # so the old configuration file still takes effect
-        ./configuration.nix
-      ];
+        nixos-cosmic = {
+            url = "github:lilyinstarlight/nixos-cosmic";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
-  };
+
+    outputs = { self, nixpkgs, nixos-cosmic, ... }@inputs: {
+        # Please replace my-nixos with your hostname
+        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+                # Import the previous configuration.nix we used,
+                # so the old configuration file still takes effect
+                ./configuration.nix
+
+                {
+                    nix.settings = {
+                        substituters = [ "https://cosmic.cachix.org" ];
+                        trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+                    };
+                }
+                nixos-cosmic.nixosModules.default
+            ];
+        };
+    };
 }
