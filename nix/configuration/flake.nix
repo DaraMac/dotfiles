3290706f5024958650fffd28a039ce9e43297b61
dmac <DaraMac@users.noreply.github.com>
2024-10-07 13:17:31 +0100

    Revert "nix: Try add k-framework flake"
    This reverts commit 616b435078783b333df54dbbd89669101ca56c64.

--- nix/configuration/flake.nix
+++ nix/configuration/flake.nix
@@ -9,10 +9,6 @@
             url = "github:lilyinstarlight/nixos-cosmic";
             inputs.nixpkgs.follows = "nixpkgs";
         };
-
-        k-framework = {
-            url = "github:runtimeverification/k";
-        };
     };
 
     outputs = { self, nixpkgs, nixos-cosmic, ... }@inputs: {
@@ -26,12 +22,11 @@
 
                 {
                     nix.settings = {
-                        substituters = [ "https://cosmic.cachix.org" "https://cache.iog.io" ];
-                        trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+                        substituters = [ "https://cosmic.cachix.org" ];
+                        trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
                     };
                 }
                 nixos-cosmic.nixosModules.default
-                k-framework.defaultPackage
             ];
         };
     };
