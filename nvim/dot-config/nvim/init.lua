71469bcc834153676f5f0f6d48748918ad0c96e9
dmac <DaraMac@users.noreply.github.com>
2026-03-26 16:42:16 +0000

    nvim: Backup old one, start new one afresh

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -1,1 +1,22 @@
-require("core")
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+vim.opt.relativenumber = true
+
+vim.opt.mouse = 'a'
+
+vim.opt.showmode = true
+
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+
+vim.opt.breakindent = true
+
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = 'number'
